2dc75e566bb5a27c3a2d09cf355b5b9f113df64b
sneha <snehabarsagade444@gmail.com>
2025-04-03 15:41:36 +0530

    allfiles

--- app
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My Hello sneha here.</h1>
-<p>My prep for Devops</p>
-
-</body>
-</html>
---
+++ app/hello.py
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My hello world</h1>
+<p>My My prep for interview</p>
+
+</body>
+</html>
